a919df4460ba1446735733ccaa70fc175cd2e46f
Reed Hillmar <reedhillmar@gmail.com>
2023-07-19 09:58:22 -0600

    feat: add Park.initialize

--- lib/park.rb
+++ lib/park.rb
@@ -1,3 +1,9 @@
 class Park
-  
+  attr_reader :name, :admission_price, :vehicles
+
+  def initialize(park_details)
+    @name = park_details[:name]
+    @admission_price = park_details[:admission_price]
+    @vehicles = []
+  end
 end
