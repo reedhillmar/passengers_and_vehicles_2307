bb60bcc181614cf9d7348e306d2df09b23b02171
Reed Hillmar <reedhillmar@gmail.com>
2023-07-19 09:26:18 -0600

    test: add test code for Vehicle.initialize

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Vehicle do
-  
+  before(:each) do
+    @vehicle = Vehicle.new("2001", "Honda", "Civic")  
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    @jude = Passenger.new({"name" => "Jude", "age" => 20}) 
+    @taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+  end
+
+  describe "#initialize" do
+    it "can exist and have details" do
+      expect(@vehicle).to be_a Vehicle
+      expect(@vehicle.year).to eq("2001")
+      expect(@vehicle.make).to eq("Honda")
+      expect(@vehicle.model).to eq("Civic")
+    end
+  end
 end
