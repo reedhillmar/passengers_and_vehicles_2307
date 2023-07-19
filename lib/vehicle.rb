b8dc407f891dd2b31102283b45dab2c74f13b9c5
Reed Hillmar <reedhillmar@gmail.com>
2023-07-19 09:35:29 -0600

    feat: add passengers as an instance variable of Vehicle

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,11 +1,12 @@
 class Vehicle
-  attr_reader :year, :make, :model
+  attr_reader :year, :make, :model, :passengers
   
   def initialize(year, make, model)
     @year = year
     @make = make
     @model = model
     @speeding = false
+    @passengers = []
   end
 
   def speeding?
